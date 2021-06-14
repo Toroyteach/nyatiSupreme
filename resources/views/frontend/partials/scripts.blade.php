25a237353b548f25108cdce9fb30b38dfc349402
Toroyteach <tonny35toro@gmail.com>
2021-06-14 19:13:34 +0300

    minor improvements

--- resources/views/frontend/partials/scripts.blade.php
+++ resources/views/frontend/partials/scripts.blade.php
@@ -1,6 +1,5 @@
 <script src="{{ asset('frontend/cssfiles/js/jquery-2.0.0.min.js') }}" type="text/javascript"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/2.9.2/cjs/popper.min.js" integrity="sha512-cccYINs21/woK1lZ16XmqaT6ohTy4BxrsgGrxnU3SEVKQACVUcU6nyPXb4meW0TG9TN9zj7OqaFK3E+ckKtmZw==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
-<script src="{{ asset('frontend/cssfiles/js/bootstrap.bundle.min.js') }}" type="text/javascript"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script><script src="{{ asset('frontend/cssfiles/js/bootstrap.bundle.min.js') }}" type="text/javascript"></script>
 <script src="{{ asset('frontend/cssfiles/plugins/fancybox/fancybox.min.js') }}" type="text/javascript"></script>
 <script src="{{ asset('frontend/cssfiles/plugins/owlcarousel/owl.carousel.min.js') }}"></script>
 <script src="{{ asset('frontend/cssfiles/js/script.js') }}" type="text/javascript"></script>
